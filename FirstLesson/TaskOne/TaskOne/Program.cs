0e4f41c9634bab89dd7ebef2bb79387c1fb4eeab
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-02 00:05:40 +0300

    Добавлен ввод информации

--- FirstLesson/TaskOne/TaskOne/Program.cs
+++ FirstLesson/TaskOne/TaskOne/Program.cs
@@ -10,6 +10,16 @@ namespace TaskOne
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Введите возрост: ");
+            string age = Console.ReadLine();
+            Console.Write("Введите рост: ");
+            string height = Console.ReadLine();
+            Console.Write("Введите вес: ");
+            string weight = Console.ReadLine();
         }
     }
 }
