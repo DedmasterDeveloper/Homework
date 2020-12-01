5503ccdf88a1e8b900d0ecee5d4bfcf74d55c087
Dedmaster <dedmaster.developer@yandex.ru>
2020-12-02 00:24:56 +0300

    Добавлен вывод

--- FirstLesson/TaskOne/TaskOne/Program.cs
+++ FirstLesson/TaskOne/TaskOne/Program.cs
@@ -20,6 +20,12 @@ namespace TaskOne
             string height = Console.ReadLine();
             Console.Write("Введите вес: ");
             string weight = Console.ReadLine();
+
+            Console.WriteLine("Имя: " + name + ", Фамилия: " + surname + ", Возрост: " + age + ", Рост: " + height + ", Вес: " + weight);
+            Console.WriteLine("Имя: {0}, Фамилия: {1}, Возрост: {2}, Рост: {3}, Вес: {4}", name, surname, age, height, weight);
+            Console.WriteLine($"Имя: {name}, Фамилия: {surname}, Возрост: {age}, Рост: {height}, Вес: {weight}");
+            Console.WriteLine("Для закрытия нажмите Enter");
+            Console.ReadLine();
         }
     }
 }
