d34181142ee7049a608c899385a19f99f1081801
DedmasterDeveloper <dedmaster.developer@yandex.ru>
2020-12-06 13:42:35 +0300

    Добавлено решение второго задания

---
+++ FirstLesson/TaskTwo/TaskTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Рассчитываем индекс массы тела");
+            Console.Write("Введите ваш рост: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ваш вес: ");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"ИМТ: {bodyMassIndex(h, m)}");
+            Console.WriteLine("Для выхода нажмите Enter");
+            Console.ReadLine();
+        }
+
+        private static double bodyMassIndex(double h, double m)
+        {
+
+            double i = new double();
+            i = m / (h * h);
+            return i;
+        }
+
+    }
+}
