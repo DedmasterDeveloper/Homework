6ce92817dd35b4753e78fb870452c09bda07cd70
DedmasterDeveloper <dedmaster.developer@yandex.ru>
2020-12-07 21:07:20 +0300

    Добавление 3 задания

---
+++ FirstLesson/TaskThree/TaskThree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите x2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ответ: {0:2f}", distance(x1,x2,y1,y2));
+
+        }
+
+        private static double distance(double x1, double x2, double y1, double y2)
+        {
+            double answer = new double();
+            answer = Math.Sqrt(Math.Pow(x2 - x1, x2) + Math.Pow(y2 - y1, y2));
+            return answer;
+        }
+    }
+}
